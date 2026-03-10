af27e886ef90a488dce4bbd52468d42cf617f154
Erik van Zijst <erik.van.zijst@gmail.com>
2026-03-10 18:17:56 +0100

    Authentik: Deleted in favor of Keycloak

--- tf/namespace.tf
+++ tf/namespace.tf
@@ -21,17 +21,6 @@ resource "kubernetes_namespace" "oauth2-proxy" {
   }
 }
 
-resource "kubernetes_namespace" "auth_system" {
-  metadata {
-    name = "auth-system"
-
-    labels = {
-      name        = "auth-system"
-      environment = local.environment
-    }
-  }
-}
-
 resource "kubernetes_namespace" "echo" {
   metadata {
     name = "echo"
